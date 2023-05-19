6555b73da6db6d9b83f75c054f82622b342a3490
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-19 11:49:38 +0530

    done

--- frontend/src/addMemberForm.jsx
+++ frontend/src/addMemberForm.jsx
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function AddMemberForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [disableInput, setDisableInput] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
-      setPasswordError('Passwords do not match');
+      setPasswordError("Passwords do not match");
       return;
     }
 
     try {
-      const response = await axios.post('http://localhost:8070/employee/add', {
+      const response = await axios.post("http://localhost:8070/employee/add", {
         name,
         email,
         phoneNumber,
@@ -27,27 +28,31 @@ function AddMemberForm() {
         confirmPassword,
       });
 
-      console.log('New employee:', response.data);
+      console.log("New employee:", response.data);
 
-      setSuccessMessage('Employee added successfully');
-      setName('');
-      setEmail('');
-      setPhoneNumber('');
-      setPassword('');
-      setConfirmPassword('');
-      setPasswordError('');
+      setSuccessMessage("Employee added successfully");
+      setName("");
+      setEmail("");
+      setPhoneNumber("");
+      setPassword("");
+      setConfirmPassword("");
+      setPasswordError("");
     } catch (error) {
-      console.error('Error adding employee:', error);
+      console.error("Error adding employee:", error);
     }
   };
 
   const handlePhoneNumberChange = (e) => {
     const input = e.target.value;
+    if (input.length >= 9) {
+      setDisableInput(true);
+    } else {
+      setDisableInput(false);
+    }
     // const isValidPhoneNumber = /^\+[0-9]*$/.test(input);
     const isValidPhoneNumber = /^0[0-9]*$/.test(input);
 
-
-    if (isValidPhoneNumber || input === '') {
+    if (isValidPhoneNumber || input === "") {
       setPhoneNumber(input);
     }
   };
@@ -55,10 +60,14 @@ function AddMemberForm() {
   return (
     <div className="container mt-4">
       <h2>Add Member Form</h2>
-      {successMessage && <div className="alert alert-success">{successMessage}</div>}
+      {successMessage && (
+        <div className="alert alert-success">{successMessage}</div>
+      )}
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name:</label>
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
           <input
             type="text"
             className="form-control"
@@ -69,7 +78,9 @@ function AddMemberForm() {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email:</label>
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
           <input
             type="email"
             className="form-control"
@@ -80,18 +91,27 @@ function AddMemberForm() {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="phoneNumber" className="form-label">Phone Number:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="phoneNumber"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-            required
-          />
+          <label htmlFor="phoneNumber" className="form-label">
+            Phone Number:
+          </label>
+          <div className="input-group">
+            <span className="input-group-text">+94</span>
+            <input
+              type="number"
+              className="form-control"
+              id="phoneNumber"
+              pattern="[0-9]{9}"
+              onChange={handlePhoneNumberChange}
+              disabled={disableInput}
+              required
+            />
+          </div>
         </div>
+
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password:</label>
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
           <input
             type="password"
             className="form-control"
@@ -103,7 +123,9 @@ function AddMemberForm() {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="confirmPassword" className="form-label">Confirm Password:</label>
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password:
+          </label>
           <input
             type="password"
             className="form-control"
@@ -115,8 +137,18 @@ function AddMemberForm() {
           />
         </div>
         {passwordError && <p className="text-danger">{passwordError}</p>}
-        <button type="submit" className="btn btn-primary">Add Member</button>
-        <button type="button" className="btn btn-secondary ms-2" onClick={() => setSuccessMessage('')}>Clear</button>
+        <button type="submit" className="btn btn-primary">
+          Add Member
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary ms-2"
+          onClick={() => {
+            setDisableInput(false);
+          }}
+        >
+          Clear
+        </button>
       </form>
     </div>
   );
